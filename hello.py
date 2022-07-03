a6409f70a586636b17e72175293b5b353842c20d
yihanseul <ihanseul731@gmail.com>
2022-07-03 16:00:43 +0900

    파이썬 코딩테스트 공부

---
+++ hello.py
@@ -0,0 +1,87 @@
+from cmd import PROMPT
+from unittest import result
+
+
+a=3
+if a>1:
+    print("a는 1보다 크다")
+
+
+for b in [1,2,3]:
+    print(b)
+
+print("while문")
+i=0
+while i<3:
+    i=i+1
+    print(i)
+
+print("함수연습")
+def add(a,b):  
+    return a+b
+print(add(3,4))
+'''
+    요거슨 주석
+'''
+
+food='"payhon is ver easy" \nhe says'
+print(food)
+
+a="apple"
+print(a*2)
+
+print(a[0:2])
+
+print ("i eat %d apples" %3)
+
+print("i eat fruits %s %d" %(a,3))
+
+print("I eat {0} apples".format("five"))
+
+print("{0:=^10}".format("hi2"))
+print(a.upper())
+
+print(a.replace("apple","banana"))
+
+print(id(a))
+
+a,b=("java","js")
+print(id(a))
+print(id(b))
+
+
+'''prompt="""
+1.add
+2.del
+3.list
+4.quit
+enter number:
+"""
+number =0
+while number !=4:
+     print(prompt)
+     number=int(input())
+
+def say():
+    print("hi")
+test=say()
+'''
+
+
+def test1(*number):
+    result =0
+    for a in number:
+        result=result+a
+    return result
+
+print(test1())
+
+
+def add_and_mul(a,b):
+    return a+b,a*b
+
+result,result1=add_and_mul(1,2)
+print(result)
+
+
+f=open("새파일.txt",'w')
---
+++ study1.py
@@ -0,0 +1,29 @@
+class Fourcal():
+    def __init__(self,first,second):
+        self.first=first
+        self.second=second
+    def add(self):
+        result=self.first+self.second
+        return result
+    def mul(self):
+        result=self.first* self.second
+        return result
+    def sub(self):
+        result =self.first-self.second
+        return result
+    def sub(self):
+        result =self.first/self.second
+        return result
+
+a=Fourcal(4,2)
+print(a.add())
+
+
+
+print(0%5)
+print(1%5)
+print(2%5)
+
+
+test=[1,2,34,4,5]
+print(max(test))
