1c8c3fbc441eae86663045b17128d76ab8bd7c23
yihanseul <ihanseul731@gmail.com>
2022-08-25 17:49:55 +0900

    최소 직사각형 완전탐색

--- 프로그래머스/lev1/최소직사각형.py
+++ 프로그래머스/lev1/최소직사각형.py
@@ -1,3 +1,21 @@
 def solution(sizes):
     answer = 0
+    sort=[]
+    minval=[]
+    maxval=[]
+
+    for i in sizes:
+        sort.append(sorted(i))
+        print(sort)
+    for i,j in sort:
+        minval.append(i)
+        maxval.append(j)
+
+    print(max(minval),max(maxval))
+    answer=max(minval)*max(maxval)
     return answer
+
+
+
+sizes=[[60, 50], [30, 70], [60, 30], [80, 40]]
+solution(sizes)
